8c349ae5ee7d383f76201c99d9d7ce6f733bd1e4
Darkipro <agamboacj@gmail.com>
2019-01-28 20:19:07 -0400

    prueba java         ../cursos/         ../desktop.ini         ../java/         ../programas/

---
+++ Proyecto1/src/Declaraciones_Operadores.java
@@ -0,0 +1,11 @@
+
+public class Declaraciones_Operadores {
+
+	public static void main(String[] args) {
+		final double apulgadas=2.54;
+		double cm=6;
+		double res=cm/apulgadas;
+		System.out.println("en " + cm + "cm hay " +res+ "pulgadas");
+	}
+
+}
---
+++ Proyecto1/src/Variables.java
@@ -0,0 +1,15 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		byte edad=46; //Declaracion e iniciacion de variables en una linea
+		System.out.println(edad);
+		edad=75;
+		System.out.println(edad);
+		/*la anterior instruccion imprime en la consola el valor de la variable
+		  edad */		 
+		 
+		
+	}
+
+}
